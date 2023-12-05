61378e3493f5dc4d8a07f074086e4ea3a9d61e25
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-12-05 06:28:06 -0600

    Welcome Page

--- lib/views/welcome.dart
+++ lib/views/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:animated_gradient/animated_gradient.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -11,14 +12,22 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: Text(
           'Hello!',
         ),
-        backgroundColor: Colors.amber,
       ),
-      body: SafeArea(
-        child: Container(),
+      body: AnimatedGradient(
+        colors: const [
+          Color(0xFF000000),
+          Color(0xFF3A6433),
+        ],
+        child: SafeArea(
+          child: Container(),
+        ),
       ),
     );
   }
