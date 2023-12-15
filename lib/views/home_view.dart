9d1701eea0b4845cc2ec624ddab8b8113b33e9ae
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-12-14 20:07:11 -0600

    Updated home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -41,7 +41,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
+    // final height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color(0xFFF6F6F6),
       extendBodyBehindAppBar: false,
