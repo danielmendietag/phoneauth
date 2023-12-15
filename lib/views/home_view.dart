f53c514cc13345745c19aafad9a611dd91028d0d
Daniel Mendieta <daniel.mendieta@gmail.com>
2023-12-14 20:06:31 -0600

    Finished home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:phoneauth/widgets/main_drawer.dart';
 import '../models/post.dart';
+import '../widgets/searchform_home.dart';
 
 class HomeView extends StatelessWidget {
   final List<Post> postInfo = [
@@ -40,30 +43,174 @@ class HomeView extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: Color(0xFFF6F6F6),
       extendBodyBehindAppBar: false,
       resizeToAvoidBottomInset: false,
+      drawer: MainDrawer(),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            backgroundColor: Colors.black,
+            forceMaterialTransparency: false,
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.white,
+            shadowColor: Colors.white,
+            surfaceTintColor: Colors.white,
             floating: true,
-            leading: Icon(
-              Icons.menu,
-              color: Colors.white,
+            pinned: false,
+            toolbarHeight: 300,
+            expandedHeight: 350,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 15, top: 10),
+              child: Builder(builder: (context) {
+                return IconButton(
+                  focusColor: Colors.transparent,
+                  splashColor: Colors.black,
+                  onPressed: () => Scaffold.of(context).openDrawer(),
+                  icon: Image.asset('assets/btnLeftMenu.png'),
+                  iconSize: 59,
+                );
+              }),
             ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.elliptical(0, 0),
-                bottomLeft: Radius.circular(0),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 20, right: 0, left: 10),
+                child: InkWell(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onTap: () {
+                    Navigator.of(context).pushReplacementNamed('/profile');
+                  },
+                  child: Ink(
+                    width: 60,
+                    height: 60,
+                    padding: const EdgeInsets.only(
+                        left: 0, top: 0, right: 20, bottom: 0),
+                    child: const CircleAvatar(
+                      radius: 25,
+                      backgroundImage:
+                      AssetImage('assets/user_assets/profile1.jpeg'),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(30),
+              child: Column(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 28.0, bottom: 20),
+                        child: Text(
+                          'Wanna play?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: 'Mulish',
+                            fontWeight: FontWeight.w800,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 28.0, bottom: 3),
+                        child: Text(
+                          'Search a golf course',
+                          style: TextStyle(
+                            color: Color(0xFF6B6B6B),
+                            fontSize: 14,
+                            fontFamily: 'Mulish',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 28.0, bottom: 20, right: 30),
+                        child: SearchFormHome(),
+                      ),
+                    ],
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: List.generate(
+                        15,
+                        (index) => Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, left: 10, right: 10, bottom: 10),
+                          child: Container(
+                            width: 55,
+                            height: 55,
+                            decoration: const ShapeDecoration(
+                                shape: OvalBorder(
+                                    side: BorderSide(
+                              width: 2,
+                              color: Color(0xFF596E79),
+                            ))),
+                            child: const CircleAvatar(
+                              radius: 22,
+                              backgroundColor:
+                                  Color.fromARGB(255, 255, 255, 255),
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: width,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF6F6F6),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 35.0, top: 20),
+                          child: Text(
+                            'Playing partners',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontFamily: 'Mulish',
+                              fontWeight: FontWeight.w700,
+                              height: 0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 35.0, top: 20),
+                          child: Icon(Icons.keyboard_arrow_up_outlined),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            actions: <Widget>[],
-            expandedHeight: 300,
           ),
           SliverList(
-            delegate: SliverChildListDelegate([
-
-            ]),
+            delegate: SliverChildBuilderDelegate(
+              childCount: 4,
+              (context, index) => buildPost(context, index),
+            ),
           ),
         ],
       ),
@@ -74,9 +221,9 @@ class HomeView extends StatelessWidget {
   Widget buildPost(BuildContext context, int index) {
     final post = postInfo[index];
     return Padding(
-      padding: const EdgeInsets.only(bottom: 24.0),
+      padding: const EdgeInsets.only(bottom: 24.0, left: 30, right: 30, top: 5),
       child: Container(
-        width: 200,
+        width: 160,
         height: 422,
         decoration: ShapeDecoration(
           color: Colors.white,
@@ -274,4 +421,3 @@ class HomeView extends StatelessWidget {
     );
   }
 }
-
